584e6b44fceb962dced2fe2ed64a544939731c02
GameSpender <misa.herceg@gmail.com>
2024-08-12 17:54:46 +0200

    Proper multithreading Still has issues with cancelling monitor.wait

--- CSMultithreading/Mailbox.cs
+++ CSMultithreading/Mailbox.cs
@@ -9,7 +9,7 @@ namespace CSMultithreading
     internal class Mailbox<T>
     {
         private Queue<T> queue;
-        Mutex mutex;
+        private Mutex mutex;
 
         public Mailbox()
         {
@@ -23,25 +23,47 @@ namespace CSMultithreading
         /// <param name="item"></param>
         public void Add(T item)
         {
-            mutex.WaitOne();
-            queue.Enqueue(item);
-            mutex.ReleaseMutex();
+            lock (queue)
+            {
+                Console.WriteLine("Writing to queue, item: {0}", item);
+                queue.Enqueue(item);
+                Monitor.Pulse(queue);
+            }
         }
 
         /// <summary>
         /// Tries to get an item from the mailbox
         /// </summary>
-        /// <param name="item">Reference to the item</param>
+        /// <param name="item">Reference    to the item</param>
         /// <returns>True if succesfull, false if mailbox is empty</returns>
         public bool TryGet(out T? item)
         {
             item = default;
-            mutex.WaitOne();
             if (queue.Count == 0) { return false; }
-            item = queue.Dequeue();
-            mutex.ReleaseMutex();
+            lock (queue)
+            { 
+                
+                item = queue.Dequeue();
+                Console.WriteLine("Reading from queue {0}", item);
+            }
             return true;
         }
 
+        /// <summary>
+        /// <c>Blocking</c> 
+        /// <para>Gets a <typeparamref name="T"/> item from the mailbox</para>
+        /// </summary>
+        /// <returns>item from mailbox</returns>
+        public T Get()
+        {
+            lock (queue)
+            {
+                if(queue.Count == 0)
+                {
+                    Monitor.Wait(queue);
+                }
+                return queue.Dequeue();
+            }
+        }
     }
 }
--- CSMultithreading/Program.cs
+++ CSMultithreading/Program.cs
@@ -1,42 +1,71 @@
 ﻿
 
+using System.Collections.Specialized;
+using System.Reflection.Metadata.Ecma335;
+
 namespace CSMultithreading
 {
     internal class Program
     {
         private static TimeSpan producerSleep = TimeSpan.FromMilliseconds(500);
 
-        internal static Mailbox<int> mailbox = new Mailbox<int>();
+        internal static Mailbox<long> mailbox = new Mailbox<long>();
 
-        internal const int maxFactorial = 12;
+        internal const int maxFactorial = 18;
         internal static Random rand = new Random();
+
+        static CancellationTokenSource tokenSource = new CancellationTokenSource();
 
 
         static void Main(string[] args)
         {
             Console.WriteLine("Multithreading app");
+            Thread producer = new Thread(() => Producer(tokenSource.Token));
+            Thread consumer = new Thread(() => Consumer(tokenSource.Token));
+
+            producer.Start();
+            consumer.Start();
+
+            Console.ReadLine();
+            tokenSource.Cancel();
+            return;
 
         }
 
-        void Consumer()
+        static void Consumer(CancellationToken cancelToken)
         {
-            int item;
+            long item, result;
             while (true)
             {
-                if (mailbox.TryGet(out item) == true)
+                if (cancelToken.IsCancellationRequested)
                 {
-                    for (int i = 0; i < item; i++)
-                    {
-
-                    }
+                    Console.WriteLine("Consumer stopping...");
+                    return;
                 }
+                item = mailbox.Get();
+                result = 1;
+                for (int i = 2; i <= item; i++)
+                {
+                    result *= i;
+                }
+                Console.WriteLine("Item: {0}, Factorial: {1}", item, result);
             }
         }
-        static void Producer()
+        static void Producer(CancellationToken cancelToken)
         {
+            long item;
             while (true)
             {
-                mailbox.Add(rand.Next(maxFactorial));
+                if (cancelToken.IsCancellationRequested)
+                {
+                    Console.WriteLine("Producer stopping...");
+                    return;
+                }
+
+                item = rand.Next(maxFactorial);
+                //Console.WriteLine("Adding item: {0}", item);
+                mailbox.Add(item);
+                
                 Thread.Sleep(producerSleep);
             }
         }
