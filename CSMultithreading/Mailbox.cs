9992e37e0cd7cab3e8f0c59e5625b4dfe20afd0a
GameSpender <misa.herceg@gmail.com>
2024-08-12 16:16:31 +0200

    Multithreading stuff

---
+++ CSMultithreading/Mailbox.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSMultithreading
+{
+    internal class Mailbox<T>
+    {
+        private Queue<T> queue;
+        Mutex mutex;
+
+        public Mailbox()
+        {
+            queue = new Queue<T>();
+            mutex = new Mutex();
+        }
+
+        /// <summary>
+        /// Adds item into the mailbox
+        /// </summary>
+        /// <param name="item"></param>
+        public void Add(T item)
+        {
+            mutex.WaitOne();
+            queue.Enqueue(item);
+            mutex.ReleaseMutex();
+        }
+
+        /// <summary>
+        /// Tries to get an item from the mailbox
+        /// </summary>
+        /// <param name="item">Reference to the item</param>
+        /// <returns>True if succesfull, false if mailbox is empty</returns>
+        public bool TryGet(out T? item)
+        {
+            item = default;
+            mutex.WaitOne();
+            if (queue.Count == 0) { return false; }
+            item = queue.Dequeue();
+            mutex.ReleaseMutex();
+            return true;
+        }
+
+    }
+}
---
+++ CSMultithreading/Program.cs
@@ -0,0 +1,45 @@
+﻿
+
+namespace CSMultithreading
+{
+    internal class Program
+    {
+        private static TimeSpan producerSleep = TimeSpan.FromMilliseconds(500);
+
+        internal static Mailbox<int> mailbox = new Mailbox<int>();
+
+        internal const int maxFactorial = 12;
+        internal static Random rand = new Random();
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Multithreading app");
+
+        }
+
+        void Consumer()
+        {
+            int item;
+            while (true)
+            {
+                if (mailbox.TryGet(out item) == true)
+                {
+                    for (int i = 0; i < item; i++)
+                    {
+
+                    }
+                }
+            }
+        }
+        static void Producer()
+        {
+            while (true)
+            {
+                mailbox.Add(rand.Next(maxFactorial));
+                Thread.Sleep(producerSleep);
+            }
+        }
+    }
+
+}
