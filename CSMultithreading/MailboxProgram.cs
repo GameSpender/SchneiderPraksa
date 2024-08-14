7c930caa1789b11e4c0d6a041c8a148b0991c74e
GameSpender <misa.herceg@gmail.com>
2024-08-14 22:51:34 +0200

    Switched to BlockingCollection for cancellation handling Exception catching doesn't work currently

---
+++ CSMultithreading/MailboxProgram.cs
@@ -0,0 +1,74 @@
+﻿
+
+using System.Collections.Specialized;
+using System.Reflection.Metadata.Ecma335;
+
+namespace CSMultithreading
+{
+    internal class MailboxProgram
+    {
+        private static TimeSpan producerSleep = TimeSpan.FromMilliseconds(500);
+
+        internal static Mailbox<long> mailbox = new Mailbox<long>();
+
+        internal const int maxFactorial = 18;
+        internal static Random rand = new Random();
+
+        static CancellationTokenSource tokenSource = new CancellationTokenSource();
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Multithreading app");
+            Thread producer = new Thread(() => Producer(tokenSource.Token));
+            Thread consumer = new Thread(() => Consumer(tokenSource.Token));
+
+            producer.Start();
+            consumer.Start();
+
+            Console.ReadLine();
+            tokenSource.Cancel();
+            return;
+
+        }
+
+        static void Consumer(CancellationToken cancelToken)
+        {
+            long item, result;
+            while (true)
+            {
+                if (cancelToken.IsCancellationRequested)
+                {
+                    Console.WriteLine("Consumer stopping...");
+                    return;
+                }
+                item = mailbox.Get();
+                result = 1;
+                for (int i = 2; i <= item; i++)
+                {
+                    result *= i;
+                }
+                Console.WriteLine("Item: {0}, Factorial: {1}", item, result);
+            }
+        }
+        static void Producer(CancellationToken cancelToken)
+        {
+            long item;
+            while (true)
+            {
+                if (cancelToken.IsCancellationRequested)
+                {
+                    Console.WriteLine("Producer stopping...");
+                    return;
+                }
+
+                item = rand.Next(maxFactorial);
+                //Console.WriteLine("Adding item: {0}", item);
+                mailbox.Add(item);
+                
+                Thread.Sleep(producerSleep);
+            }
+        }
+    }
+
+}
--- CSMultithreading/Program.cs
+++ CSMultithreading/Program.cs
@@ -1,7 +1,9 @@
-﻿
-
-using System.Collections.Specialized;
-using System.Reflection.Metadata.Ecma335;
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace CSMultithreading
 {
@@ -9,29 +11,39 @@ namespace CSMultithreading
     {
         private static TimeSpan producerSleep = TimeSpan.FromMilliseconds(500);
 
-        internal static Mailbox<long> mailbox = new Mailbox<long>();
-
+        static BlockingCollection<long> blocks = new BlockingCollection<long>();
         internal const int maxFactorial = 18;
         internal static Random rand = new Random();
 
         static CancellationTokenSource tokenSource = new CancellationTokenSource();
-
-
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Multithreading app");
-            Thread producer = new Thread(() => Producer(tokenSource.Token));
-            Thread consumer = new Thread(() => Consumer(tokenSource.Token));
-
-            producer.Start();
-            consumer.Start();
+            Console.WriteLine("Hello World!");
+            Task prod = Task.Run(() => Producer(tokenSource.Token));
+            Task cons = Task.Run(() => Consumer(tokenSource.Token));
+            Task consExtra = Task.Run(() => ConsumerException(tokenSource.Token), tokenSource.Token);
 
             Console.ReadLine();
             tokenSource.Cancel();
+
+            try
+            {
+                await prod;
+                await cons;
+                await consExtra;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Task exception: {0}", ex);
+            }
+
+            tokenSource.Dispose();
             return;
-
         }
-
+        /// <summary>
+        /// Vadi i računa faktorijel vrednosti sa <see cref="blocks">blocks</see>
+        /// </summary>
+        /// <param name="token"></param>
         static void Consumer(CancellationToken cancelToken)
         {
             long item, result;
@@ -42,7 +54,18 @@ namespace CSMultithreading
                     Console.WriteLine("Consumer stopping...");
                     return;
                 }
-                item = mailbox.Get();
+
+                try
+                {
+                    item = blocks.Take(cancelToken);
+                }
+                catch (OperationCanceledException)
+                {
+                    Console.WriteLine("Consumer stopped during Take...");
+                    return;
+                }
+                
+
                 result = 1;
                 for (int i = 2; i <= item; i++)
                 {
@@ -51,6 +74,34 @@ namespace CSMultithreading
                 Console.WriteLine("Item: {0}, Factorial: {1}", item, result);
             }
         }
+
+        /// <summary>
+        /// Vadi i računa faktorijel vrednosti sa <see cref="blocks">blocks</see>
+        /// </summary>
+        /// <remarks>Baci exception kada je prekinut pomoću <paramref name="token"/></remarks>
+        /// <param name="token"></param>
+        /// <exception cref="OperationCanceledException"/>
+        static void ConsumerException(CancellationToken token)
+        {
+            long item, result;
+            while (true)
+            {
+                item = blocks.Take(token);
+
+                result = 1;
+                for (int i = 2; i <= item; i++)
+                {
+                    result *= i;
+                }
+                Console.WriteLine("No Cancel Item: {0}, Factorial: {1}", item, result);
+            }
+        }
+
+
+        /// <summary>
+        /// Dodaje nasumičan broj manji od <see cref="maxFactorial">maxFactorial</see> u <see cref="blocks">blocks</see>
+        /// </summary>
+        /// <param name="cancelToken"></param>
         static void Producer(CancellationToken cancelToken)
         {
             long item;
@@ -63,12 +114,21 @@ namespace CSMultithreading
                 }
 
                 item = rand.Next(maxFactorial);
-                //Console.WriteLine("Adding item: {0}", item);
-                mailbox.Add(item);
+                Console.WriteLine("Adding item: {0}", item);
+                try
+                {
+                    blocks.Add(item, cancelToken);
+                }
+                catch (OperationCanceledException)
+                {
+                    Console.WriteLine("Producer stopped during Add...");
+                    return;
+                }
                 
+
                 Thread.Sleep(producerSleep);
             }
         }
-    }
 
+    }
 }
